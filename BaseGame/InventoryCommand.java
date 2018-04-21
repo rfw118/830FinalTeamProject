5314ac8b8ad8a959fbf3816d2ec78b40ca4abc14
rfw118 <36180359+rfw118@users.noreply.github.com>
2018-04-21 19:29:28 -0400

    Updated the subscribe methodology

--- BaseGame/InventoryCommand.java
+++ BaseGame/InventoryCommand.java
@@ -5,6 +5,11 @@ import UI.MyGame;
 
 public class InventoryCommand implements Commandable {
 
+	public InventoryCommand(MyGame g)
+	{
+		g.Subscribe(this);
+	}
+	
 	@Override
 	public void doCommand(MyGame g) {
 		// TODO Auto-generated method stub
@@ -16,5 +21,5 @@ public class InventoryCommand implements Commandable {
 		
 		return s.toUpperCase().equals("INVENTORY"); //returns true for match and false for no match.
 	}
-
+	
 }
