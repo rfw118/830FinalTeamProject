c916f76d572e8b3002f04d136a0b83ac4e0e5a75
rfw118 <36180359+rfw118@users.noreply.github.com>
2018-04-21 15:48:08 -0400

    Updated the base game.

--- UI/MyGame.java
+++ UI/MyGame.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import BaseGame.Inventory;
+import BaseGame.InventoryCommand;
 
 public class MyGame {
 /*
@@ -15,17 +16,19 @@ public class MyGame {
 	public QuitCommand QuitMe = new QuitCommand();
 	public ListCommand ListMe = new ListCommand();
 	public SortCommand SortMe = new SortCommand();
+	public InventoryCommand InvMe = new InventoryCommand();
 	public List<Commandable> UserCommands = new ArrayList<Commandable>();
 	public Inventory BeltAndBackPack = new Inventory();
 	private Boolean bolQuit = false;
 	private String sSortOrder = "";
 	private String sUserInput = "";
+	private String sCR = System.getProperty("line.separator"); //Carriage Return
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyGame MBL = new MyGame();
-		MBL.Run();
+		MyGame mG = new MyGame();
+		mG.Run();
 		
 	}
 	
@@ -39,10 +42,19 @@ public class MyGame {
 		Subscribe(this.ListMe);
 		Subscribe(this.SortMe);		
 		
-		UserInterface.UserOutput("Welcome!  This is the book library!");
-		ProcessUserInputCommands("HELP");
-		UserInterface.UserOutput("How would you like the list to be sorted?  <Author | Title | Year | NoSort>");
-		
+		UserInterface.UserOutput("Welcome!  You are now playing Escape From Endor!" + sCR + sCR);
+
+		UserInterface.UserOutput(sCR + "You are the AT-ST driver pulled, by Chewbacca, from the AT-ST and commandeered by Chewbacca and two Ewoks and you find yourself lying on the floor of the forest moon" + sCR 
+				+ " of Endor not far from the shield generator command bunker. You are injured to a certain degree from your fall have been overlooked by the puny band of filthy rebel forces and Ewoks.  " + sCR 
+				+ " You are presumed KIA and forgotten by the captured Imperial Forces. You remain uncaptured and unseen by the Ewoks and rebels when you see the shield generator command bunker fall to rebel forces," + sCR 
+				+ " the shield generator destroyed and the Death Star II destroyed as well. \r\n \r\n" + 
+				"In order to remain alive and uncaptured with any possibility of returning to remaining Imperial Forces, you must evade capture or death by avoiding Ewoks, rebel forces and rebel forces who are " + sCR 
+				+ "arriving after the destruction of Death Star. You need to obtain provisions, a weapon(s), a map, a medical kit, communication equipment and possibly a surface transport in order to survive long enough" + sCR 
+				+ " to depart this infested dirtball called a forest moon. \r\n \r\n" + 
+				"How will you know where to go? What are your means of departing the forest moon? How will you communicate to remaining Imperial Forces?  If you do NOT have a means of departing the moon " + sCR
+				+ "or communicating with remaining Imperial Forces you will surely perish...\r\n" + "\r\n"
+				);
+		ProcessUserInputCommands("HELP");		
 		while( bolQuit == false)
 		{
 			sUserInput = inputScanner.next();
